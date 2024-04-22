60f1607852081619b2eeb6c727b69274d8e49cd2
Wanottah <wanottah@gmail.com>
2024-04-22 08:41:53 +0100

    Initial commit

---
+++ scripts/cancellations.js
@@ -0,0 +1,8 @@
+     mail = "<A HREF='mai"
+     to = "lto:"
+     emailname = "cancellations"
+     emailserver = "stringythings.com"
+     at = "@"
+     document.write(mail + to + emailname + at + emailserver + "'>");
+     document.write(emailname + at + emailserver);
+     document.write("</a>");
